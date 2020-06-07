dc6161eb268637d20c850109c97f061e9cc4cbeb
jeremyg89 <62123543+jeremyg89@users.noreply.github.com>
2020-06-06 23:54:36 -0700

    Added single and double click listeners, Word creation, and checking the word against the api

--- WordWizard/app/src/main/java/com/example/wordwizard/DictionaryAPI.java
+++ WordWizard/app/src/main/java/com/example/wordwizard/DictionaryAPI.java
@@ -12,11 +12,35 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.Buffer;
 
-public class Oxford extends AsyncTask<String, Integer, String> {
+public class DictionaryAPI extends AsyncTask<String, Integer, String> {
 
     @Override
     protected String doInBackground(String... params) {
-        //API ID and Key used to make a connection
+
+        try{
+            //create a URL using the passed parameters
+            URL url = new URL(params[0]);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            //create the bufferReader and StringBuilder to gather the information from the API
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String line = null;
+
+            //Loop to write the information pulled from the API connection formatted in JSON
+            while ((line = reader.readLine()) != null){
+                stringBuilder.append(line + "\n");
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+    /*
+    @Override
+    protected String doInBackground(String... params) {
         final String appID = "01480e9c";
         final String appKey = "1da9051e74233120c76d09e3ae2e7c30";
 
@@ -45,18 +69,39 @@ public class Oxford extends AsyncTask<String, Integer, String> {
             return e.toString();
         }
     }
+*/
     @Override
+    protected void onPostExecute(String result){
+        super.onPostExecute(result);
+        //write the results to the textview will later just show that the word is not real
+        if (result.contains("[{\"meta\":"))
+        {
+            NewGame.currentWord.setText(NewGame.word + " is a real word. Points Points Points.");
+            NewGame.word = "";
+        }
+        else
+        {
+            NewGame.currentWord.setText("Not a real word.");
+            NewGame.word = "";
+        }
+    }
+    /*
+   @Override
     protected void onPostExecute(String result){
         super.onPostExecute(result);
         //write the results to the textview will later just show that the word is not real
         if (result.contains("java.io.FileNotFoundException: https://od-api.oxforddictionaries.com/api/v2/lemmas/en/"))
         {
-            MainActivity.dataView.setText("Not a real word.");
+            NewGame.currentWord.setText("Not a real word.");
+            NewGame.word = "";
         }
         else
         {
-            MainActivity.dataView.setText(result);
+            NewGame.currentWord.setText(NewGame.word + " is a real word. Points Points Points.");
+            NewGame.word = "";
         }
     }
+
+     */
 }
 
