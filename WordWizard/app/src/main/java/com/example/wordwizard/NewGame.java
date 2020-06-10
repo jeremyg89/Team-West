17321ec083ae509ee50e57a4ecaaa9b50406257a
jeremyg89 <62123543+jeremyg89@users.noreply.github.com>
2020-06-09 18:37:23 -0700

    Added todays scrum meeting and corrected the onClick code

--- WordWizard/app/src/main/java/com/example/wordwizard/NewGame.java
+++ WordWizard/app/src/main/java/com/example/wordwizard/NewGame.java
@@ -2,9 +2,12 @@ package com.example.wordwizard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Layout;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -16,7 +19,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class NewGame extends AppCompatActivity implements View.OnClickListener{
-
     //string array to hold the random letters
     String[] randomLetters = new String[16];
     private static final int NUM_ROWS = 4;
@@ -31,6 +33,7 @@ public class NewGame extends AppCompatActivity implements View.OnClickListener{
 
     public static TextView currentWord;
 
+    boolean doubleTap = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,24 +44,26 @@ public class NewGame extends AppCompatActivity implements View.OnClickListener{
         relativeLayout.setOnClickListener(this);
         populateGrid();
     }
-
     @Override
     public void onClick(final View v) {
         click++;
         //new handler used to check if the button has been clicked twice
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
+            @SuppressLint("ResourceType")
             @Override
             public void run() {
                 //if the button has been clicked once it will add the letter to the chain
                 if (click == 1) {
-                    if (wordCount <= 16) {
+                    if (wordCount <= 16 ) {
                         //add the letter to the word
-                        word = word + randomLetters[v.getId()];
-                        v.setEnabled(false);
-                        wordCount++;
-                        //display the current letter selection
-                        currentWord.setText(word);
+                        if (v.getId() <= 16) {
+                            word = word + randomLetters[v.getId()];
+                            v.setEnabled(false);
+                            wordCount++;
+                            //display the current letter selection
+                            currentWord.setText(word);
+                        }
                     }
                 }
                 //if a double click is detected the word will be checked using the API
@@ -84,6 +89,7 @@ public class NewGame extends AppCompatActivity implements View.OnClickListener{
     private void populateGrid(){
         letterGenerator.letterGen(randomLetters);
         TableLayout table = (TableLayout) findViewById(R.id.tableGrid);
+        table.setOnClickListener(this);
         int count = 0;
         for (int row = 0; row < NUM_ROWS; row++){
             TableRow tableROW = new TableRow(this);
@@ -97,7 +103,6 @@ public class NewGame extends AppCompatActivity implements View.OnClickListener{
                 button.setText(randomLetters[count].toString());
                 button.setBackgroundResource(R.drawable.ic_ww_btn_grid_a);
                 button.setPadding(0,0,0,0);
-
                 button.setOnClickListener(this);
                 count++;
                 tableROW.addView(button);
