c6c611ff5972ca7f5aa949d10131658d805ea351
jharay <61820041+jharay@users.noreply.github.com>
2020-07-06 16:48:16 -0700

    database diagram updates, started letter scramble code

---
+++ WordWizard/app/src/main/java/com/example/wordwizard/letterScramble.java
@@ -0,0 +1,14 @@
+package com.example.wordwizard;
+
+public class letterScramble {
+
+    //generates random number between 1-8, 1-4 equals columns, 5-8 equals rows
+    int min = 1;
+    int max = 8;
+    int line = (int) ((Math.random()*((max-min)+1))+min);
+
+
+
+
+
+}
