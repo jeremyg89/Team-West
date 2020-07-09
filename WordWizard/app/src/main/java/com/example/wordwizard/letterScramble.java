37eebfea39aa0aecd054aaf024ac3992afec7fff
jharay <61820041+jharay@users.noreply.github.com>
2020-07-09 16:52:16 -0700

    changed quantity

--- WordWizard/app/src/main/java/com/example/wordwizard/letterScramble.java
+++ WordWizard/app/src/main/java/com/example/wordwizard/letterScramble.java
@@ -4,7 +4,7 @@ public class letterScramble {
 
     //generates random number between 1-8, 1-4 equals columns, 5-8 equals rows
     int min = 1;
-    int max = 8;
+    int max = 4;
     int line = (int) ((Math.random()*((max-min)+1))+min);
 
 
