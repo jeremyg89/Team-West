bb3eb1515fc16f4d37bf5dea25868b380e86961c
jharay <61820041+jharay@users.noreply.github.com>
2020-07-10 21:23:03 -0700

    Update letterScramble.java

--- WordWizard/app/src/main/java/com/example/wordwizard/letterScramble.java
+++ WordWizard/app/src/main/java/com/example/wordwizard/letterScramble.java
@@ -3,26 +3,53 @@ package com.example.wordwizard;
 import java.util.Random;
 
 public class letterScramble<line> {
+    int line =0;
+    char l;
 
     //generates random number between 1-4 to choose row
-    int min = 1;
-    int max = 4;
 
-    int line = (int) ((Math.random() * ((max - min) + 1)) + min);
 
     public int getLine() {
+        int min = 1;
+        int max = 4;
+        int line = (int) ((Math.random() * ((max - min) + 1)) + min);
         return line;
     }
 
-    for (int i=0; i< 4; i++){
-        getLine();
-        NewGame.row = line;
-        NewGame.col = 0;
-        int min =65;
-        int max =90;
+    public int getChar(){
+        int min = 65;
+        int max = 90;
         int letter = (int) ((Math.random() * ((max - min) + 1)) + min);
-        char l = (char)letter;
-        NewGame.col = col++;
+        char l = (char) letter;
+        return l;
+    }
+    public int chooseButton(){
+        switch (line){
+            case 1:
+                NewGame.btn(1);
+                break;
+            case 2:
+                NewGame.btn(4);
+                break;
+            case 3:
+                NewGame.btn(9);
+                break;
+            case 4:
+                NewGame.btn(13);
+                break;
+
+
+            for (int i=0; i< 4; i++) {
+                int x = l;
+                NewGame.btn(x);
+                x++;
+
+
+        }
+
+
+    }
+    
     }
 
     }
