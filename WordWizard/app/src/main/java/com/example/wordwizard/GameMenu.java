a1c44a19e9584a0f7db1c2f04d4f8b5c96b892b0
jeremyg89 <62123543+jeremyg89@users.noreply.github.com>
2020-07-15 15:22:11 -0700

    Updated the user information on the respective pages

--- WordWizard/app/src/main/java/com/example/wordwizard/GameMenu.java
+++ WordWizard/app/src/main/java/com/example/wordwizard/GameMenu.java
@@ -3,6 +3,7 @@ package com.example.wordwizard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,9 @@ public class GameMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
+
+        TextView nickname = findViewById(R.id.txtNickname);
+        nickname.setText(PopSignIn.nickname.toString());
     }
     public void newGame(View v){
         //calls the NewGame activity
--- WordWizard/app/src/main/java/com/example/wordwizard/PopAccount.java
+++ WordWizard/app/src/main/java/com/example/wordwizard/PopAccount.java
@@ -3,6 +3,7 @@ package com.example.wordwizard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -20,5 +21,14 @@ public class PopAccount extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.9), (int)(height*.7));
+
+        TextView nickname = findViewById(R.id.txtAccountNickname);
+        nickname.setText(PopSignIn.nickname.toString());
+
+        TextView email = findViewById(R.id.txtAccountEmail);
+        email.setText(PopSignIn.email.toString());
+
+        TextView id = findViewById(R.id.txtUserId);
+        id.setText(Integer.toString(PopSignIn.id));
     }
 }
